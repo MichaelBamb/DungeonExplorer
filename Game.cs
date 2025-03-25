fba639ceb3605ac78dc4d142255f6d8d541edd59
MichaelBamb <michaelbamberger@outlook.com>
2025-03-25 12:52:17 +0000

    Code updated with suggestions from reviews

--- Game.cs
+++ Game.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Diagnostics.Eventing.Reader;
 using System.Linq.Expressions;
 using System.Media;
+using System.Runtime.InteropServices;
 
 namespace DungeonExplorer
 {
@@ -20,6 +21,7 @@ namespace DungeonExplorer
 
             List<string> newItems = new List<string>();
             newItems.Add("Sword");
+            newItems.Add("Shield");
             Room starting_room = new Room("This is the starting room and I'm terrible at descriptions.", newItems);
             currentRoom = starting_room;
         }
@@ -33,27 +35,27 @@ namespace DungeonExplorer
                 string optionsString = "\nOptions:\nView Player Stats (Stats)\n";
                 bool itemAvailable = false;
                 Console.WriteLine(currentRoom.GetDescription());
-                if (currentRoom.GetItemCount() > 0)
+                if (currentRoom.GetItemCount() > 0) // Checks if any items are present in the room, if so adds an option to get them into the string
                 {
                     optionsString += "Get Item (Get)\n";
                     itemAvailable = true;
                 }
 
-                string userInput = InputCheckStrings(optionsString + "Exit\n");
-                if (userInput.Contains("Stats"))
+                string userInput = InputCheckStrings(optionsString + "Exit\n"); // Uses InputCheckStrings() method for input validation
+                if (userInput.Contains("Stats")) // Displays the user's stats
                 {
                     Console.WriteLine("Name = {0}\nHealth = {1}\nInventory = {2}", player.Name, player.Health, player.InventoryContents());
                 }
-                else if (userInput.StartsWith("Get") && itemAvailable == true)
+                else if (userInput.StartsWith("Get") && itemAvailable == true) // Checks if items are available and the user enters Get
                 {
-                    List<string> items = currentRoom.GetItems();
+                    List<string> items = currentRoom.GetItems(); // Gets items in the current room and displays them to the user
                     Console.WriteLine("\nAvailable Items:\n");
                     foreach (string item in items)
                     {
                         Console.WriteLine(item);
                     }
 
-                    userInput = InputCheckStrings("\nWhich would you like to pick up?\n");
+                    userInput = InputCheckStrings("\nWhat would you like to pick up?\n"); // Gets user input and moves item from room to inventory if applicable
                     if (items.Contains(userInput))
                     {
                         currentRoom.RemoveItem(userInput);
@@ -63,10 +65,19 @@ namespace DungeonExplorer
                     else
                         Console.WriteLine("That was not an available item.");
                 }
+                else if (userInput.Contains("Exit")) // Checks if player wants to exit the game
+                {
+                    playing = false;
+                    continue;
+                }
+                else
+                {
+                    Console.WriteLine("That was not a valid input, please try again\nPlease note that the phrases are Case Sensetive!\n");
+                }
             }
         }
 
-        public string InputCheckStrings(string displayString)
+        public string InputCheckStrings(string displayString) // Function used to validate user input
         {
             while (true)
             {
